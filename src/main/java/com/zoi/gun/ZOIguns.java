2097a497f423cc45a778954d60e46809b0b42032
ZOI <zoizoichannel@gmail.com>
2020-01-23 15:50:09 +0900

    コメント追加 登録処理まで

--- src/main/java/com/zoi/gun/ZOIguns.java
+++ src/main/java/com/zoi/gun/ZOIguns.java
@@ -1,6 +1,7 @@
 package com.zoi.gun;
 
 import java.io.IOException;
+
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.config.Configuration;
@@ -16,32 +17,76 @@ import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import defeatedcrow.flamethrower.common.CommonProxyF;
 
+/*
+ * @Modアノテーションです。
+ * MODのID、表示名、起動に必要な他MODなどを記述するところ。
+ */
 @Mod(modid = "ZOIguns", name = "ZOIguns", version = "1.7.10_0.0.1", dependencies = "required*after:Forge@[10.13.4.1558,)")
 
 public class ZOIguns {
+
+    // プロキシの登録
     @SidedProxy(clientSide = "com.zoi.gun.client.ClientProxyF", serverSide = "com.zoi.gun.common.CommonProxyF")
     public static CommonProxyF proxy;
+
+    // インスタンスの生成
+    // @InstanceのStringにはMODのIDを入れます。
     @Instance("ZOIguns")
     public static FlameCore instance;
+
+    /* Loggerクラス
+     * 必須ではありませんが、エラー箇所の調査時など、一時的にコンソールログを出したいときに使用しています。
+     * たとえば、FlameCore.logger.info("ログの内容の文字列")のような形でログを出します。
+     */
     public static Logger logger = LogManager.getLogger("ZOIguns");
 
+    // AK47のインスタンス。
     public static Item AK47;
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
+        /*
+         * FMLPreInitializationEventです。
+         * このメソッドをはじめとしたローディングイベントは、マインクラフトの起動時、Forgeによって所定のタイミングで呼ばれます。
+         * 実際どんなことが起きているかはコンソールログにでてくるので、
+         * 興味があればVersions/logsフォルダにあるfml-client-latest.logを眺めてみると良いです。
+         *
+         * アイテムやブロックの登録などは、基本的にはここで行われます。
+         */
+        // conf
         Configuration cfg = new Configuration(event.getSuggestedConfigurationFile());
         try {
             cfg.load();
+            /*
+             * Configの項目の生成と読み込み。
+             * この段階では、まだ項目はありません。
+             */
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
             cfg.save();
         }
+
+        // アイテムやブロックの登録はこのあたりで行う予定。
+        // itemの登録。
+        this.AK47 = new ItemIgnis().setCreativeTab(CreativeTabs.tabCombat).setUnlocalizedName("dcsflame.flamethrower");
+        GameRegistry.registerItem(flamethrower, "ZOIguns.AK47");
     }
+
     @EventHandler
     public void init(FMLInitializationEvent event) throws IOException {
+        /*
+         * FMLInitializationEventです。全てのMODのPreInitの後、順にInitが呼ばれます。
+         * 私の場合、ここではEntity、TileEntity、Render関係、ForgeやFMLのイベント登録などを行っています。
+         * レシピもこのあたりで登録することが多いですが、このMODの場合は諸事情によりPostInitで登録します。
+         */
     }
+
     @EventHandler
     public void postInit(FMLPostInitializationEvent event) {
+        /*
+         * FMLPostInitializationEventです。全てのMODのInitとIMCEventの後、順に呼ばれます。
+         * アイテムやブロックなど色々なMODの追加物が揃っていることが多いため、MOD間の連携要素の読み込みによく利用されます。
+         */
     }
 }
